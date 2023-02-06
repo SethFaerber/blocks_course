696cee14ee447d39a637e480dbc6a4c62dc54746
Seth Faerber <seth.faerber@ramseysolutions.com>
2023-02-06 16:46:06 -0600

    Lesson 7 complete: yield to add

--- execute_around.rb
+++ execute_around.rb
@@ -21,3 +21,43 @@ tag(:ul) do
   tag(:li) { "It shines!"}
   tag(:li) { "It mesmerizes!"}
 end
+
+# Debugging exercise
+
+def with_debugging
+  puts "Got Here!"
+  result = yield
+  puts "Result was #{result}"
+end
+
+with_debugging do
+  magic_number = (23 - Time.now.hour) * Math::PI
+end
+
+# Expectation
+
+def with_expectation(expectation)
+  puts "Running test..."
+   if expectation == yield
+     puts "Passed!"
+   else
+     puts "Failed!"
+     puts "Expected #{expectation}, but got #{yield}."
+   end
+end
+
+with_expectation(5) { 2 + 2 }
+with_expectation(4) { 2 + 2 }
+with_expectation(5) { "Hi there!" }
+
+# yield is fully run every time it is called.
+
+run_times = 0
+
+def run_it
+  yield
+  yield
+  yield
+end
+
+run_it { puts run_times += 1 }
